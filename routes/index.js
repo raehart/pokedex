9a65f4a2a12587796c19e78c87ee484414b2646c
LawlietBlack <kenneth.black@live.com>
2015-07-25 06:57:54 -0700

    movesets complete for first 22 pokemon. Added code for pokedex page at root '/'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var pokedex = require("../public/js/pokedex.js")
 /* GET home page. */
+
+var hoennDex = {}
+
+for(var i=0; i<450; i++) {
+	if(pokedex.hoenn[i] !== undefined) {
+		var pokemon = pokedex.hoenn[i];
+		hoennDex[i] = pokemon.name
+	}
+}
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var pokedex = {"pokedex": hoennDex}
+	console.dir(pokedex)
+  res.render('index', pokedex);
 });
 
 module.exports = router;
