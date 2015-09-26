4a35c3b15cec6efb91e97ff436b7832314787f6e
Brigette Eckert <brigette.eckert@live.com>
2015-09-26 16:55:17 -0700

    added place holders to dex page, got image url for sprite to be returned on get data function run

--- js/app.js
+++ js/app.js
@@ -19,17 +19,21 @@ angular.module('PokedexApp', ['ngRoute'])
 	$scope.dexnum = $routeParams.dexnum;
 	$scope.root = "http://pokeapi.co"
 	var url = "http://pokeapi.co/api/v1/pokemon/" + $scope.dexnum + "/";
-	$http.get(url).success(function(data) {
+		$http.get(url).success(function(data) {
 		$scope.pokemon = data;
-		console.log($scope.pokemon)
-
+		// console.log($scope.root + $scope.pokemon.sprites[0].resource_uri);
+		$scope.getData($scope.pokemon.sprites[0].resource_uri);
 	})
-
 
 	$scope.getData = function(uri) {
 		var url = $scope.root + uri;
 		$http.get(url).success(function(data) {
-			console.log(data)
+			$scope.uriData = data;
+			console.log($scope.uriData);
+			if($scope.uriData.hasOwnProperty("image")) {
+				console.log($scope.root + $scope.uriData.image);	
+			}
+			//add if statements to return other urls as needed
 		})
 	}
 
